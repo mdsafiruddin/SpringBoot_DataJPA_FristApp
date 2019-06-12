ee5374e1115b7fa6439ac5321f2d6f8bb1255dd1
MD <MD@Safu>
2019-06-12 08:51:03 +0530

    initial commit

---
+++ src/main/java/com/app/SpringBootDataJpaFristAppApplication.java
@@ -0,0 +1,17 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaFristAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaFristAppApplication.class, args);
+	
+		//System.out.println("from Starter class");
+		
+		
+	}
+
+}
