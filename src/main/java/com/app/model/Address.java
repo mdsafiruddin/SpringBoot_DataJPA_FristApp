e01e6cad68bd7001655e0d534af747615ab0a2c0
Md SAFI <mds.info1996@gmail.com>
2025-04-27 00:17:26 +0530

    modified address field

--- src/main/java/com/app/model/Address.java
+++ src/main/java/com/app/model/Address.java
@@ -3,25 +3,53 @@ package com.app.model;
 public class Address {
 	private String loName;
 	private String addNo;
+	private String street;
+	
+	public Address(String loName, String addNo, String street) {
+		super();
+		this.loName = loName;
+		this.addNo = addNo;
+		this.street = street;
+	}
+
+	
+	public String getLoName() {
+		return loName;
+	}
+
+
+	public void setLoName(String loName) {
+		this.loName = loName;
+	}
+
 
 	public String getAddNo() {
 		return addNo;
 	}
 
+
 	public void setAddNo(String addNo) {
 		this.addNo = addNo;
 	}
 
-	public Address(String addNo) {
-		super();
-		this.addNo = addNo;
+
+	public String getStreet() {
+		return street;
 	}
+
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
 
 	@Override
 	public String toString() {
-		return "Address [addNo=" + addNo + "]";
+		return "Address [loName=" + loName + ", addNo=" + addNo + ", street=" + street + "]";
 	}
 
+	
+	
 
 	
 	
